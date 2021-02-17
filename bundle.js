bb3855302914aff69e074150ffd2c145112a0660
Wandkleister <zimmermann.d90@gmail.com>
2021-02-17 12:48:45 +0000

    Deploying to gh-pages from @ sinnerschrader/sinnerschrader-reboot@8071bcb47cf46ca065102cd9244d91decb94c376 🚀

--- bundle.js
+++ bundle.js
@@ -1667,7 +1667,7 @@
     constructor() {
       const sliderOptions = {
         type: "slider",
-        perView: 3,
+        perView: 2,
         gap: 24,
         peek: {
           before: 0,
@@ -1677,30 +1677,25 @@
         rewind: false,
         animationTimingFunc: "ease",
         breakpoints: {
-          1024: {
-            perView: 1
+          1e3: {
+            perView: 1,
+            gap: 16
+          },
+          600: {
+            perView: 1,
+            gap: 12
           }
         }
       };
       this.init();
       this.mountSlider(sliderOptions);
-      this.bindEvents();
     }
     init() {
       this.slider = {};
-      this.circleText = document.querySelector(".slider__circle-text");
-      this.rotationAngle = 0;
-    }
-    bindEvents() {
-      this.slider.on("run", this.rotateCircleText.bind(this));
     }
     mountSlider(options) {
       this.slider = new glide_esm_default(".glide", options);
       this.slider.mount();
-    }
-    rotateCircleText() {
-      this.rotationAngle += 60;
-      this.circleText.setAttribute("style", `transform: rotate(${this.rotationAngle}deg)`);
     }
   };
 

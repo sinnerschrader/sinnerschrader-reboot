af6a99408829c694064b9e63847240cbfd911016
David Zimmermann <david.zimmermann@sinnerschrader.com>
2021-02-17 16:24:08 +0100

    feat(video): customize player, adjust behaviour on mobile

--- _includes/js/video.js
+++ _includes/js/video.js
@@ -5,6 +5,7 @@ class Video {
 
 	init() {
 		this.video = {
+			previewElement: document.querySelector(".work__video-preview-loop"),
 			playButton: document.querySelector(".work__video-media-control"),
 			videoElement: document.querySelector(".work__video-element"),
 		};
@@ -17,7 +18,32 @@ class Video {
 	}
 
 	togglePlay() {
+		const videoPreloadAttr = this.video.videoElement.getAttribute("preload");
+
+		if (videoPreloadAttr === "none") {
+			this.displayVideo();
+		}
+
 		this.video.videoElement.paused === true ? this.video.videoElement.play() : this.video.videoElement.pause();
+		this.video.playButton.classList.toggle("is-playing");
+	}
+
+	displayVideo() {
+		this.video.previewElement.classList.add("is-hidden");
+		this.video.videoElement.classList.add("is-visible");
+		this.video.videoElement.setAttribute("preload", "auto");
+
+		if (this.isTouchDevice()) {
+			this.fullscreenOnMobile();
+		}
+	}
+
+	fullscreenOnMobile() {
+		this.video.videoElement.requestFullscreen().error("Request to go in fullscreen unsuccessful");
+	}
+
+	isTouchDevice() {
+		return "ontouchstart" in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
 	}
 }
 

189f9bdc53ecb59349efbe01af35ec5484c3cccc
David Zimmermann <david.zimmermann@sinnerschrader.com>
2021-02-22 16:36:02 +0100

    feat(offering): heading animatioN

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -1,9 +1,11 @@
 import { Slider } from "./slider.js";
 import { Locations } from "./locations";
 import { Video } from "./video.js";
+import { OfferingHeader } from "./offering-header";
 
 (() => {
 	new Locations();
 	new Slider();
 	new Video();
+	new OfferingHeader();
 })();

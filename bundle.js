c28fc5f0461cc4658f3cda7af134a841b9b85909
tam-kis <kis.tamara91@gmail.com>
2021-02-23 09:58:09 +0000

    Deploying to gh-pages from @ sinnerschrader/sinnerschrader-reboot@4d4c94680802c075b09f03a3b4522e6e663e910b 🚀

--- bundle.js
+++ bundle.js
@@ -4868,11 +4868,34 @@
     }
   };
 
+  // _includes/js/animate-slidein.js
+  var SlideIn = class {
+    titles = document.querySelectorAll(".slide-in");
+    constructor() {
+      this.init();
+    }
+    init() {
+      let observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.intersectionRatio > 0) {
+            entry.target.style.animation = `slidein 0.5s ${entry.target.dataset.delay} forwards ease`;
+          } else {
+            entry.target.style.animation = "none";
+          }
+        });
+      });
+      this.titles.forEach((title) => {
+        observer.observe(title);
+      });
+    }
+  };
+
   // _includes/js/main.js
   (() => {
     new Locations();
     new Slider();
     new BackgroundScrollAnimation();
     new Video();
+    new SlideIn();
   })();
 })();

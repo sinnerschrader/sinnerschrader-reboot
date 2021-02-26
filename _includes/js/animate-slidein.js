a0a69816ac7ac1a181fa3aa58a219edda59f5dbc
trinegl <trine.grotkjaer-lund@sinnerschrader.com>
2021-02-26 11:46:40 +0100

    feat: add a11y media query to animations

--- _includes/js/animate-slidein.js
+++ _includes/js/animate-slidein.js
@@ -1,15 +1,17 @@
 export class SlideIn {
 	titles = document.querySelectorAll(".slide-in");
+	showAnimations = window.matchMedia("(prefers-reduced-motion: no-preference)");
 	constructor() {
 		this.init();
 	}
 	init() {
 		let observer = new IntersectionObserver((entries) => {
 			entries.forEach((entry) => {
-				if (entry.intersectionRatio > 0) {
+				if (entry.intersectionRatio > 0 && this.showAnimations.matches) {
 					entry.target.style.animation = `slidein 0.5s ${entry.target.dataset.delay} forwards ease`;
 				} else {
 					entry.target.style.animation = "none";
+					entry.target.style.opacity = 1;
 				}
 			});
 		});

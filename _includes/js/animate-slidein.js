d780685b1e2cb8cade89a8187204ca4d21331655
trinegl <trine.grotkjaer-lund@sinnerschrader.com>
2021-03-03 12:23:37 +0100

    fix: correct opacity in animated headlines

--- _includes/js/animate-slidein.js
+++ _includes/js/animate-slidein.js
@@ -1,17 +1,22 @@
 export class SlideIn {
 	titles = document.querySelectorAll(".slide-in");
-	showAnimations = window.matchMedia("(prefers-reduced-motion: no-preference)");
+	hideAnimations = window.matchMedia("(prefers-reduced-motion: reduce)");
 	constructor() {
 		this.init();
 	}
 	init() {
 		let observer = new IntersectionObserver((entries) => {
 			entries.forEach((entry) => {
-				if (entry.intersectionRatio > 0 && this.showAnimations.matches) {
+				if (this.hideAnimations.matches) {
+					entry.target.style.animation = "none";
+					entry.target.style.opacity = 1;
+					return;
+				}
+
+				if (entry.intersectionRatio > 0) {
 					entry.target.style.animation = `slidein 0.5s ${entry.target.dataset.delay} forwards ease`;
 				} else {
 					entry.target.style.animation = "none";
-					entry.target.style.opacity = 1;
 				}
 			});
 		});

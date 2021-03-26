900dcfcd5f35ecff926d5ee53ab29bf5a2e19558
Dario Beck <dario.beck@sinnerschrader.com>
2021-03-26 10:39:45 +0100

    feat: add styling

--- src/_includes/js/filter-checkbox.js
+++ src/_includes/js/filter-checkbox.js
@@ -1,8 +1,14 @@
-export function filterCheckboxHandler(checkboxId) {
-	var checkbox = document.querySelector("#" + checkboxId);
-	if (checkbox.checked) {
-		checkbox.classList.add("filter-checkbox--checked");
-	} else {
-		checkbox.classList.remove("filter-checkbox--checked");
+export class FilterCheckbox {
+	constructor() {}
+
+	filterCheckboxHandler(checkboxId) {
+		console.log(checkboxId);
+		var checkbox = document.querySelector("#checkbox-" + checkboxId);
+		var input = document.querySelector("#" + checkboxId);
+		if (input.checked) {
+			checkbox.classList.add("filter-checkbox--checked");
+		} else {
+			checkbox.classList.remove("filter-checkbox--checked");
+		}
 	}
 }

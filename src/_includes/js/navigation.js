b6c0cc4c97af0d40cdda834a5b296e05c262d365
David Zimmermann <david.zimmermann@sinnerschrader.com>
2021-03-26 17:26:19 +0100

    feat(navigation): design qa improvements part 2

--- src/_includes/js/navigation.js
+++ src/_includes/js/navigation.js
@@ -4,6 +4,8 @@ class Navigation {
 	bodyOverlayClass = "is-no-scroll";
 	mobileActiveClass = "is-mobile-active";
 	topClass = "is-top";
+	transparentClass = "is-transparent";
+	lightBackgroundClass = "is-light";
 	animateInClass = "is-animate-in";
 	animateOutClass = "is-animate-out";
 	lastScrollTop = 0;
@@ -20,16 +22,10 @@ class Navigation {
 	}
 
 	bindEvents() {
-		this.onLoadPosition();
+		this.detectScroll();
 
 		window.addEventListener("scroll", throttle(this.detectScroll.bind(this), 50));
 		this.mobileToggleBtn.addEventListener("click", this.toggleMobilePanel.bind(this));
-	}
-
-	onLoadPosition() {
-		if (window.pageYOffset <= 150) {
-			this.navigationContainer.classList.add(this.topClass);
-		}
 	}
 
 	toggleMobilePanel() {
@@ -40,21 +36,39 @@ class Navigation {
 	detectScroll() {
 		let scrollTop = window.pageYOffset;
 
-		if (scrollTop > this.lastScrollTop) {
+		this.animateInOut(scrollTop);
+		this.detectPageTop(scrollTop);
+		this.toggleTransparentClass(scrollTop);
+
+		this.lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+	}
+
+	animateInOut(scrollPosition) {
+		if (scrollPosition > this.lastScrollTop) {
 			this.navigationContainer.classList.remove(this.animateInClass);
 			this.navigationContainer.classList.add(this.animateOutClass);
 		} else {
 			this.navigationContainer.classList.remove(this.animateOutClass);
 			this.navigationContainer.classList.add(this.animateInClass);
 		}
+	}
 
-		if (scrollTop <= 150) {
+	detectPageTop(scrollPosition) {
+		if (scrollPosition <= 0) {
 			this.navigationContainer.classList.add(this.topClass);
 		} else {
 			this.navigationContainer.classList.remove(this.topClass);
 		}
+	}
 
-		this.lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+	toggleTransparentClass(scrollPosition) {
+		if (scrollPosition <= 150) {
+			this.navigationContainer.classList.remove(this.lightBackgroundClass);
+			this.navigationContainer.classList.add(this.transparentClass);
+		} else {
+			this.navigationContainer.classList.remove(this.transparentClass);
+			this.navigationContainer.classList.add(this.lightBackgroundClass);
+		}
 	}
 }
 

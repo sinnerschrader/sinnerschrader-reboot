cf6f4614cb870090e8859cbd11b55e715060f4e2
Feli <felicitas.kugland@sinnerschrader.com>
2021-04-26 12:01:12 +0200

    fix: workflows

--- src/_includes/js/careerContact.js
+++ src/_includes/js/careerContact.js
@@ -30,7 +30,7 @@ export class CareerContact {
 	sentform(event) {
 		event.preventDefault();
 		const { name, contact, profile, privacy, email, contactPerson } = event.target;
-		const endpoint = "https://xr9bha0zvh.execute-api.eu-central-1.amazonaws.com";
+		const endpoint = "https://7ric1lpsg1.execute-api.eu-central-1.amazonaws.com";
 		// We use JSON.stringify here so the data can be sent as a string via HTTP
 		const body = JSON.stringify({
 			senderName: name.value,

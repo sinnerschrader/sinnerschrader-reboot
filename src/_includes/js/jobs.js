7627ba15c9ba5ab89b7ba3a5aa2d251eeb47621b
Moritz Behnke <moritz.behnke@sinnerschrader.com>
2021-08-16 14:14:17 +0200

    job filter accessibility (#401)
    * fix: prevent keyboard reachability for hidden element in image text teaser
    
    * fix: keyboard navigation for filters
    
    * fix: add aria attributes to filter toggle

--- src/_includes/js/jobs.js
+++ src/_includes/js/jobs.js
@@ -128,6 +128,28 @@ export class FilterList {
 	// open close the filter bar
 	toggleFilterBarOpen() {
 		this.controls.classList.toggle("is-open");
+
+		if (this.controls.classList.contains("is-open")) {
+			this.controls.querySelectorAll("input").forEach((input) => input.removeAttribute("tabindex"));
+			this.controls.querySelector("#js-clear-filter").removeAttribute("tabindex");
+			this.controls.querySelector("#js-apply-filter").removeAttribute("tabindex");
+
+			this.controls.querySelectorAll("[aria-expanded]").forEach((button) => button.setAttribute("aria-expanded", true));
+
+			this.controls.querySelector("#js-job-filter-bar .job-filter-wrapper button.js-toggle-filter-bar--mobile").removeAttribute("tabindex");
+			this.controls.querySelector("#js-job-filter-bar header button.js-toggle-filter-bar--mobile").setAttribute("tabindex", -1);
+		} else {
+			this.controls.querySelectorAll("input").forEach((input) => input.setAttribute("tabindex", -1));
+			this.controls.querySelector("#js-apply-filter").setAttribute("tabindex", -1);
+			this.controls.querySelector("#js-clear-filter").setAttribute("tabindex", -1);
+
+			this.controls.querySelectorAll("[aria-expanded]").forEach((button) => button.setAttribute("aria-expanded", false));
+
+			this.controls
+				.querySelector("#js-job-filter-bar .job-filter-wrapper button.js-toggle-filter-bar--mobile")
+				.setAttribute("tabindex", -1);
+			this.controls.querySelector("#js-job-filter-bar header button.js-toggle-filter-bar--mobile").removeAttribute("tabindex");
+		}
 	}
 
 	// Loops through HTML Categories in the filter section and creates an filter object

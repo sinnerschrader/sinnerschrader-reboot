d74baacaad311a6f371184ac96a9441835dfdce6
s2-pauher <pauline.hering@sinnerschrader.com>
2021-09-07 08:40:42 +0200

    fix: profile link contact form sliding animation

--- src/_includes/js/profile-link-contact-form.js
+++ src/_includes/js/profile-link-contact-form.js
@@ -1,37 +1,33 @@
 export class ProfileLinkContactForm {
 	constructor() {
 		this.container = document.querySelector(".profile-link-contact-form");
-
 		if (!this.container) return;
-		console.log("whatever");
 
-		this.defaultContent = this.container.querySelector('[data-js-item="profile-link-contact-form-content"]');
-		this.hiddenContent = this.container.querySelector('[data-js-item="profile-link-contact-form-content-hidden"]');
-		this.forwardBtn = this.defaultContent.querySelector('[data-js-atom="profile-link-contact-form-forward"]');
-		this.backwardBtn = this.hiddenContent.querySelector('[data-js-atom="profile-link-contact-form-backward"]');
+		this.firstPage = this.container.querySelector('[data-js-item="profile-link-contact-form-content"]');
+		this.secondPage = this.container.querySelector('[data-js-item="profile-link-contact-form-content-hidden"]');
+		this.forwardBtn = this.firstPage.querySelector('[data-js-atom="profile-link-contact-form-forward"]');
+		this.backwardBtn = this.secondPage.querySelector('[data-js-atom="profile-link-contact-form-backward"]');
+		this.firstPage.classList.add("active-state");
 
 		this.bindEvents();
 	}
 
 	bindEvents() {
-		this.forwardBtn.addEventListener("click", this.toggleHiddenContent.bind(this));
-		this.backwardBtn.addEventListener("click", this.toggleHiddenContent.bind(this));
+		this.forwardBtn.addEventListener("click", this.togglePages.bind(this));
+		this.backwardBtn.addEventListener("click", this.togglePages.bind(this));
 	}
 
-	toggleHiddenContent() {
-		this.hiddenContent.classList.toggle("is-active");
-		this.defaultContent.classList.toggle("is-hidden");
-
-		if (this.hiddenContent.classList.contains("is-active")) {
-			this.hiddenContent.getElementsByTagName("button")[0].removeAttribute("tabindex");
-		} else {
-			this.hiddenContent.getElementsByTagName("button")[0].setAttribute("tabindex", -1);
-		}
-
-		if (this.defaultContent.classList.contains("is-hidden")) {
-			this.defaultContent.getElementsByTagName("button")[0].setAttribute("tabindex", -1);
-		} else {
-			this.defaultContent.getElementsByTagName("button")[0].removeAttribute("tabindex");
+	togglePages() {
+		if (this.firstPage.classList.contains("active-state")) {
+			this.firstPage.classList.remove("active-state");
+			this.secondPage.classList.add("active-state");
+			this.firstPage.getElementsByTagName("button")[0].setAttribute("tabindex", -1);
+			this.secondPage.getElementsByTagName("button")[0].removeAttribute("tabindex");
+		} else if (this.secondPage.classList.contains("active-state")) {
+			this.secondPage.classList.remove("active-state");
+			this.firstPage.classList.add("active-state");
+			this.secondPage.getElementsByTagName("button")[0].setAttribute("tabindex", -1);
+			this.firstPage.getElementsByTagName("button")[0].removeAttribute("tabindex");
 		}
 	}
 }

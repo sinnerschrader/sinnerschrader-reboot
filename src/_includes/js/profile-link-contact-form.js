aa85223b515b46d4c4e828cc2f2e0e9b196c5fae
s2-pauher <pauline.hering@sinnerschrader.com>
2021-09-14 11:07:59 +0200

    fix: change naming and set initial class of active

--- src/_includes/js/profile-link-contact-form.js
+++ src/_includes/js/profile-link-contact-form.js
@@ -7,7 +7,6 @@ export class ProfileLinkContactForm {
 		this.secondPage = this.container.querySelector('[data-js-item="profile-link-contact-form-content-hidden"]');
 		this.forwardBtn = this.firstPage.querySelector('[data-js-atom="profile-link-contact-form-forward"]');
 		this.backwardBtn = this.secondPage.querySelector('[data-js-atom="profile-link-contact-form-backward"]');
-		this.firstPage.classList.add("active-state");
 
 		this.bindEvents();
 	}
@@ -18,14 +17,14 @@ export class ProfileLinkContactForm {
 	}
 
 	togglePages() {
-		if (this.firstPage.classList.contains("active-state")) {
-			this.firstPage.classList.remove("active-state");
-			this.secondPage.classList.add("active-state");
+		if (this.firstPage.classList.contains("active")) {
+			this.firstPage.classList.remove("active");
+			this.secondPage.classList.add("active");
 			this.firstPage.getElementsByTagName("button")[0].setAttribute("tabindex", -1);
 			this.secondPage.getElementsByTagName("button")[0].removeAttribute("tabindex");
-		} else if (this.secondPage.classList.contains("active-state")) {
-			this.secondPage.classList.remove("active-state");
-			this.firstPage.classList.add("active-state");
+		} else if (this.secondPage.classList.contains("active")) {
+			this.secondPage.classList.remove("active");
+			this.firstPage.classList.add("active");
 			this.secondPage.getElementsByTagName("button")[0].setAttribute("tabindex", -1);
 			this.firstPage.getElementsByTagName("button")[0].removeAttribute("tabindex");
 		}

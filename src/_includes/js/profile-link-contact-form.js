116e9a9d1ae7519d5aa17dcf82624a3a2ef0bb4e
s2-pauher <pauline.hering@sinnerschrader.com>
2021-11-01 16:53:38 +0100

    feat: form validation

--- src/_includes/js/profile-link-contact-form.js
+++ src/_includes/js/profile-link-contact-form.js
@@ -7,43 +7,67 @@ export class ProfileLinkContactForm {
 		this.secondPage = this.container.querySelector('[data-js-item="profile-link-contact-form-content-hidden"]');
 		this.forwardBtn = this.firstPage.querySelector('[data-js-atom="profile-link-contact-form-forward"]');
 		this.backwardBtn = this.secondPage.querySelector('[data-js-atom="profile-link-contact-form-backward"]');
-		this.submitBtn = this.secondPage.querySelector('[data-js-atom="profile-link-contact-form-submit"]');
-		this.input = this.secondPage.querySelector('[data-js-atom="profile-link-contact-form-input"]');
-		this.checkbox = this.secondPage.querySelector('[data-js-atom="profile-link-contact-form-checkbox"]');
 		this.captchaContainer = this.secondPage.querySelector('[data-js-atom="profile-link-contact-form-captcha-container"]');
+		this.form = this.secondPage.querySelector("form");
+
 		this.bindEvents();
 	}
 
 	bindEvents() {
-		this.forwardBtn.addEventListener("click", this.togglePages.bind(this));
-		this.backwardBtn.addEventListener("click", this.togglePages.bind(this));
-		this.submitBtn.addEventListener("click", this.sendHandleEmail.bind(this));
-		this.captchaWidget = hcaptcha.render(this.captchaContainer, {
+		this.captchaWidgetId = hcaptcha.render(this.captchaContainer, {
 			sitekey: "555327a5-0262-4a9e-90c5-b257351d1317",
 			theme: "dark",
 			callback: this.onCaptchaSuccess.bind(this),
+			"expired-callback": this.onCaptchaExpired.bind(this),
+		});
+
+		this.forwardBtn.addEventListener("click", this.togglePages.bind(this));
+		this.backwardBtn.addEventListener("click", this.togglePages.bind(this));
+
+		this.form.addEventListener("submit", (event) => {
+			event.preventDefault();
+			this.sendProfileLinkEmail();
 		});
 	}
 
 	onCaptchaSuccess(response) {
-		this.captchaResponse = response;
+		this.captureResponse = response;
+		this.updateSubmitButtonDisabledState();
+	}
+
+	onCaptchaExpired() {
+		this.captureResponse = undefined;
+		this.updateSubmitButtonDisabledState();
 	}
 
 	emailSent() {
-		hcaptcha.reset(this.captchaWidget);
+		hcaptcha.reset(this.captchaWidgetId);
 	}
 
 	emailFailed() {}
 
-	async sendHandleEmail() {
-		const data = { payload: { handle: this.input.value }, type: "only_handle", captcha: this.captchaResponse };
-		fetch("https://k741x3mcij.execute-api.eu-central-1.amazonaws.com/prod/emailer", {
-			body: JSON.stringify(data),
-			mode: "no-cors",
-			method: "POST",
-		})
-			.then(this.emailSent)
-			.catch(this.emailFailed);
+	updateSubmitButtonDisabledState() {
+		if (this.captureResponse) {
+			this.form.querySelector("button").removeAttribute("disabled");
+		} else {
+			this.form.querySelector("button").setAttribute("disabled", true);
+		}
+	}
+
+	async sendProfileLinkEmail() {
+		const formData = new FormData(this.form);
+
+		if (this.captureResponse) {
+			const data = { payload: { handle: formData.get("profileLink") }, type: "only_handle", captcha: this.captureResponse };
+
+			fetch("https://k741x3mcij.execute-api.eu-central-1.amazonaws.com/prod/emailer", {
+				body: JSON.stringify(data),
+				mode: "no-cors",
+				method: "POST",
+			})
+				.then(this.emailSent)
+				.catch(this.emailFailed);
+		}
 	}
 
 	togglePages() {
